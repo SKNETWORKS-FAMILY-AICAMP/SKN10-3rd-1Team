535ead5c9292d4a48285ca2bcba0e326eef306db
yuuunong <ccbb15379@naver.com>
2025-04-11 14:11:21 +0900

    langchain 모듈 개발

--- web/LLM/llm.py
+++ web/LLM/llm.py
@@ -1,16 +1,31 @@
-import streamlit as st
-from langchain_community.tools import TavilySearchResults
+from langchain_chroma import Chroma
+from langchain_community.chat_models import ChatOllama
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import time
+
+def search_similarity(query, vector_store:Chroma, k:int=3) -> str:
+    """Search for similar documents in the vector store."""
+    return vector_store.similarity_search(query, k=k)[0].page_content
+
+def get_prompt() -> PromptTemplate:
+    """Get the prompt for the chat model."""
+    return PromptTemplate(
+        template="""
+        You are a helpful assistant. Answer the question based on the context provided.
+        Job description: {context}
+        Self-Instruction: {self_instruction}
+        Question: {query}
+        Answer the Korean question in Korean.:
+        """,
+        input_variables=["context", "self_instruction", "query"]
+    )
+
+def get_answer(prompt:PromptTemplate, ollama:ChatOllama, context:str, self_instruction:str, query:str) :
+    """Get the answer from the chat model."""
+    chain = prompt | ollama | StrOutputParser()
+    for token in chain.stream({"context": context, "self_instruction": self_instruction, "query": query}):
+        yield token
+        time.sleep(0.05) 
 
 
-def tavily_search(query: str, num_results: int = 5) -> list:
-    """
-    Perform a search using Tavily and return the results.
-    """
-    # Initialize the TavilySearchResults tool
-    tavily_search = TavilySearchResults()
-
-    # Perform the search
-    results = tavily_search.run(query=query, num_results=num_results)
-
-    # Return the search results
-    return results
