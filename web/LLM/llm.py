a16a0b2a022625bf984eca70074975a985f18af9
yuuunong <ccbb15379@naver.com>
2025-04-11 16:23:26 +0900

    테스트 성공

--- web/LLM/llm.py
+++ web/LLM/llm.py
@@ -3,6 +3,34 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import time
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+
+def load_vector_store() -> Chroma:
+    pdf_files = [
+        "../../data/2024년 하반기 3급 신입사원 채용 직무소개서.pdf",
+        "../../data/삼성전자 DS부문 24하 공채 직무기술서.pdf"
+    ]
+    docs = []
+    for path in pdf_files:
+        loader = PyPDFLoader(path)
+        docs.extend(loader.load())
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=600, chunk_overlap=0)
+    
+    split_doc1 = text_splitter.split_documents(docs)
+
+    # 저장할 경로 지정
+    DB_PATH = "../../data/chroma_db"
+
+    # 문서를 디스크에 저장합니다. 저장시 persist_directory에 저장할 경로를 지정합니다.
+    db = Chroma.from_documents(
+        split_doc1, OpenAIEmbeddings(), persist_directory=DB_PATH, collection_name="my_db"
+    )
+
+    return db
 
 def search_similarity(query, vector_store:Chroma, k:int=3) -> str:
     """Search for similar documents in the vector store."""
